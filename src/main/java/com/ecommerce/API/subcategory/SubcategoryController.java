bf19e00385bc0baa53f97101a557fb8c94d14c5e
Kevin <kevin005005@gmail.com>
2024-10-16 10:41:19 +0200

    Get by subcategory id added

--- src/main/java/com/ecommerce/API/subcategory/SubcategoryController.java
+++ src/main/java/com/ecommerce/API/subcategory/SubcategoryController.java
@@ -1,10 +1,15 @@
 package com.ecommerce.API.subcategory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class SubcategoryController {
@@ -19,5 +24,11 @@ public class SubcategoryController {
     @GetMapping("/subcategories")
     public List<Subcategory> getSubCategories() {
         return this.subcategoryService.getSubcategories();
+    }
+
+    @GetMapping("/subcategory/{id}")
+    public ResponseEntity<Subcategory> getSubCategories(@PathVariable long id) {
+        Optional<Subcategory> subcategory = this.subcategoryService.getSubcategory(id);
+        return subcategory.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
     }
 }
--- src/main/java/com/ecommerce/API/subcategory/SubcategoryService.java
+++ src/main/java/com/ecommerce/API/subcategory/SubcategoryService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SubcategoryService {
@@ -16,5 +17,9 @@ public class SubcategoryService {
 
     public List<Subcategory> getSubcategories() {
         return this.subcategoryRepository.findAll();
+    }
+
+    public Optional<Subcategory> getSubcategory(long id) {
+        return this.subcategoryRepository.findAll().stream().filter(s -> s.getId() == id).findFirst();
     }
 }
